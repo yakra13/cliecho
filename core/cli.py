e3c0da21bf8860d38fbfbb7e9118db1bb5afcfce
jz <jz@jz.com>
2025-11-21 21:21:05 +0000

    asd

--- core/cli.py
+++ core/cli.py
@@ -5,4 +5,20 @@ from dispatcher import Dispatcher
 class CLI:
 
     def run(self):
+        while True:
+            # show modules
+            # search "*" (need tags or search name and description?)
+            # help
+            # use "module"
+            # describe "module"
+            # exit
+            # > show options
+            # > set "option" "value"
+            # > unset "option"
+            # > run
+            # > info
+            # > save settings "label" (store modules current settings value)
+            # > show settings (shows saved settings for current module)
+            # > load "label"
+            # > back
         pass
